7e0ff90272313ad9f5cad89976aba2f9f76b8e28
Alex <sanakostrov04@gmail.com>
2021-11-06 13:51:37 +0200

    Change TK_1.py: add construction try-expect

--- TK_1.py
+++ TK_1.py
@@ -1,5 +1,8 @@
 def input_data_to_list(size_list: int):
-    user_list = []
-    for i in range(size_list):
-        user_list.append(int(input(f'Input number in list[{i}]: ')))
-    return user_list
+    try:
+        user_list = []
+        for i in range(size_list):
+            user_list.append(int(input(f'Input number in list[{i}]: ')))
+        return user_list
+    except ValueError:
+        print('You input wrong value, list is not be created')
